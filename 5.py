18fd71a489fb8ec2836444b0c19f97dcc908f685
Sandesh Tiwari <sandeshtiwari@live.com>
2018-01-31 01:26:27 -0600

    feat: Add method to find the mth element and add test cases

--- 5.py
+++ 5.py
@@ -16,3 +16,34 @@ class LinkedList(object):
         else:
             self.head = new_element
         self.length += 1
+        #print(new_element.data)
+        #print(self.length)
+    def question5(self, first, m):
+        count = 0
+        if(self.length == 0):
+            return None
+        if(self.length < m):
+            return "Please enter a valid number to fetch node"
+        while(first.next) and count <= (self.length - m - 1):
+            count += 1
+            #print(str(count) + " " + str(first.data))
+            first = first.next
+            #print(first.data)
+        return first.data
+
+e1 = Node(1)
+e2 = Node(2)
+e3 = Node(3)
+e4 = Node(4)
+
+li = LinkedList()
+print(li.question5(e1,1))
+# output - None
+li.push(e1)
+li.push(e2)
+li.push(e3)
+li.push(e4)
+print(li.question5(e1,2))
+# output - 3
+print(li.question5(e1,5))
+# output - Please enter a valid number to fetch node
