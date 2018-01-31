b70a5c23d4e47d4afa8ddcb62cd3729364e07fbe
Sandesh Tiwari <sandeshtiwari@live.com>
2018-01-31 01:11:33 -0600

    feat: Add function to check possible substrings

--- 1.py
+++ 1.py
@@ -1,3 +1,16 @@
+# Function to check all the possible substrings of the original_string
+def question1(original_string, check_string):
+    check_string_len = len(check_string)
+    original_string_len = len(original_string)
+    # making a list of the check_string
+    sorted_check_string = list(check_string)
+    # Using Quick sort to sort the string - O(n*log(n))
+    sorted_check_string.sort()
+    for i in range(original_string_len - check_string_len + 1):
+        if check_anagram(original_string[i: i+check_string_len], sorted_check_string):
+            return True
+    return False
+
 # Function to check if string2 is an anagram of string1
 def check_anagram(string1, string2):
     string1 = list(string1)
