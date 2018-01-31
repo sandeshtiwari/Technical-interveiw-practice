4836e723d49df4dd71fc5c0a9c936b8f280a85b7
Sandesh Tiwari <sandeshtiwari@live.com>
2018-01-31 01:25:00 -0600

    feat: Add LinkedList class and push method

--- 5.py
+++ 5.py
@@ -2,3 +2,17 @@ class Node(object):
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class LinkedList(object):
+    length = 0
+    head = None
+    def push(self, new_element):
+        global length
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+        self.length += 1
