998bbd808242c9e2dd243ad13132b435fadb3889
Sandesh Tiwari <sandeshtiwari@live.com>
2018-01-31 01:15:39 -0600

    feat: Add 2.py file and function to find longest palindrome

---
+++ 2.py
@@ -0,0 +1,17 @@
+# function to find the longest palindromic string
+def question2(string):
+    if string == None:
+        return ""
+    n = len(string)
+    longest = 0
+    left_index = 0
+    right_index = 0
+    for i in range(0, n):
+        for j in range(i + 1, n + 1):
+            check_str = string[i:j]
+            if is_palindrome(check_str) and len(check_str) > longest:
+                longest = len(check_str)
+                left_index = i
+                right_index = j
+    longest_pal = string[left_index:right_index]
+    return longest_pal
