832edb410c1e26c918d0d553cf62119232d7d45a
Sandesh Tiwari <sandeshtiwari@live.com>
2018-02-07 00:56:17 -0600

    docs: Add question description

--- 1.py
+++ 1.py
@@ -1,3 +1,9 @@
+'''
+Given two strings s and t, determine whether some anagram of t is a substring
+of s. For example: if s = "udacity" and t = "ad", then the function returns
+True. Your function definition should look like: question1(s, t) and return
+a boolean True or False.
+'''
 # Function to check all the possible substrings of the original_string
 def question1(original_string, check_string):
     check_string_len = len(check_string)
